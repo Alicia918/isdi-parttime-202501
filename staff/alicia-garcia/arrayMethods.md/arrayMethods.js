98e53d662889e67b080118d878b2581fc231e390
alicia garcia <aligargon99@gmail.com>
2025-02-09 21:08:48 +0100

    modify array V_1 #6

---
+++ staff/alicia-garcia/arrayMethods.md/arrayMethods.js
@@ -0,0 +1,10 @@
+// método pop modificado. 
+
+//debugger
+var arr = ["angel", "clown", "mandarin", "sturgeon"];
+
+var popped = arr[arr.length - 1]
+ arr[3] -= popped
+
+console.log(popped)
+console.log(arr)
