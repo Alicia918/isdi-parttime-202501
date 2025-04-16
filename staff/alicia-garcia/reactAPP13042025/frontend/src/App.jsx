ab7eb9e6a265efd0922240da4c69aa0654b5d143
alicia garcia <aligargon99@gmail.com>
2025-04-16 09:11:28 +0200

    add 16042025 abre app

--- staff/alicia-garcia/reactAPP13042025/frontend/src/App.jsx
+++ staff/alicia-garcia/reactAPP13042025/frontend/src/App.jsx
@@ -1,11 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
+//import './index.css';
 import './App.css';
-
-//import './pages/register.css';
-
-
 
 function App() {
   return (
@@ -16,7 +12,10 @@ function App() {
         <img className="icon" src="https://cdn-icons-png.freepik.com/512/11818/11818739.png?ga=GA1.1.1811276553.1739812746" />
             <div className="titleLanding">
                 <h>YOGA TERAPIA</h>
-            </div>           
+            </div>  
+            <div>
+                <button onClick={() => alert('¡Hola!')} className='joinButton'>COMENZAMOS</button>
+            </div>          
             
         </div>
     </div>
@@ -26,12 +25,17 @@ function App() {
 export default App;
 
 
-/* 
-<div>
-                {showPage && <Btn
-                    className={'joinButton'} 
-                    onClick={handleNavClick}    
-                    btnContent ={'COMENZAMOS'}
-                    />                  
-                }             
-            </div>        */
+
+
+/*
+
+export default App;
+/*<div>
+
+    {showPage && <Btn
+        className={'joinButton'} 
+        onClick={handleNavClick}    
+        btnContent ={'COMENZAMOS'}
+        />                  
+    }             
+</div>  */
--- staff/alicia-garcia/reactAPP13042025/frontend/src/components/Btn.jsx
+++ staff/alicia-garcia/reactAPP13042025/frontend/src/components/Btn.jsx
@@ -1,8 +1,10 @@
 /*Este componente cumple un proposito muy parecido al de la función de createButton que tenemos hecha en nuestra app de js vanilla*/
 
 const Btn = ({ btnContent, btnCallback, className}) => {
-    return <button className={className} onClick={btnCallback}>{btnContent}
+    return <button className={className} 
+                     onClick={btnCallback}>{btnContent}
     
     </button>
     
 }
+export default Btn;
